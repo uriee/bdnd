5a1f60c3cb24bf36b363456f16b3c142098e0aac
Asaf <asaf.meir.barziv@gmail.com>
2021-01-20 09:58:12 +0200

    added routes

---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Board = () => (
+  <div>
+    <h2>Board</h2>
+  </div>
+);
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,153 +1,32 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import styled from "@emotion/styled";
-import { Global, css } from "@emotion/core";
-import { colors } from "@atlaskit/theme";
-import Column from "./column";
-import reorder, { reorderQuoteMap } from "./reorder";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { authorQuoteMap } from "./data";
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const ParentContainer = styled.div`
-  /* height: ${({ height }) => height}; */
-  /* overflow-x: hidden;
-  overflow-y: auto; */
-`;
+import About from "./components/About";
+import Home from "./components/Home";
+import Board from "./components/board/Board";
 
-const Container = styled.div`
-  background-color: ${colors.B100};
-  /* min-height: 100vh; */
-  /* like display:flex but will allow bleeding over the window width */
-  /* min-width: 100vw; */
-  display: inline-flex;
-`;
+const BasicExample = () =>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/board">Board</Link>
+        </li>
+      </ul>
 
-class Board extends Component {
-  /* eslint-disable react/sort-comp */
-  static defaultProps = {
-    isCombineEnabled: false
-  };
+      <hr />
 
-  state = {
-    columns: this.props.initial,
-    ordered: Object.keys(this.props.initial)
-  };
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/Board" component={Board} />
+    </div>
+  </Router>;
 
-  boardRef;
-
-  onDragEnd = result => {
-    if (result.combine) {
-      if (result.type === "COLUMN") {
-        const shallow = [...this.state.ordered];
-        shallow.splice(result.source.index, 1);
-        this.setState({ ordered: shallow });
-        return;
-      }
-
-      const column = this.state.columns[result.source.droppableId];
-      const withQuoteRemoved = [...column];
-      withQuoteRemoved.splice(result.source.index, 1);
-      const columns = {
-        ...this.state.columns,
-        [result.source.droppableId]: withQuoteRemoved
-      };
-      this.setState({ columns });
-      return;
-    }
-
-    // dropped nowhere
-    if (!result.destination) {
-      return;
-    }
-
-    const source = result.source;
-    const destination = result.destination;
-
-    // did not move anywhere - can bail early
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    ) {
-      return;
-    }
-
-    // reordering column
-    if (result.type === "COLUMN") {
-      const ordered = reorder(
-        this.state.ordered,
-        source.index,
-        destination.index
-      );
-
-      this.setState({
-        ordered
-      });
-
-      return;
-    }
-
-    const data = reorderQuoteMap({
-      quoteMap: this.state.columns,
-      source,
-      destination
-    });
-
-    this.setState({
-      columns: data.quoteMap
-    });
-  };
-
-  render() {
-    const columns = this.state.columns;
-    const ordered = this.state.ordered;
-    const { containerHeight } = this.props;
-
-    const board = (
-      <Droppable
-        droppableId="board"
-        type="COLUMN"
-        direction="horizontal"
-        ignoreContainerClipping={Boolean(containerHeight)}
-        isCombineEnabled={this.props.isCombineEnabled}
-      >
-        {provided => (
-          <Container ref={provided.innerRef} {...provided.droppableProps}>
-            {ordered.map((key, index) => (
-              <Column
-                key={key}
-                index={index}
-                title={key}
-                quotes={columns[key]}
-                isScrollable={this.props.withScrollableColumns}
-                isCombineEnabled={this.props.isCombineEnabled}
-              />
-            ))}
-            {provided.placeholder}
-          </Container>
-        )}
-      </Droppable>
-    );
-
-    return (
-      <React.Fragment>
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          {containerHeight ? (
-            <ParentContainer height={containerHeight}>{board}</ParentContainer>
-          ) : (
-            board
-          )}
-        </DragDropContext>
-        <Global
-          styles={css`
-            body {
-              background: ${colors.B200};
-            }
-          `}
-        />
-      </React.Fragment>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Board initial={authorQuoteMap} />, rootElement);
+render(<BasicExample />, document.getElementById("root"));
